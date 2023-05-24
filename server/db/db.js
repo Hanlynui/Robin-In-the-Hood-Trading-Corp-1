91089e868a32d09f586a02c3f560ee9071d4e22f
HanLynUI <114448369+Hanlynui@users.noreply.github.com>
2023-05-23 20:52:39 -0400

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -12,14 +12,14 @@ if (process.env.LOGGING === "true") {
   delete config.logging;
 }
 
-//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-// if (process.env.DATABASE_URL) {
-//   config.dialectOptions = {
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   };
-// }
+https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
 
 const db = new Sequelize(
   `postgresql://Hanlynui:v2_44fB7_SCAWCRAh4cGPrjQmR2AvDXr@db.bit.io:5432/Hanlynui/robin-in-the-hood`,
