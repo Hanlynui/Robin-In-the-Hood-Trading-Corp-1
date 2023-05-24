c51305739a4c5ac51f73b8b6f103d122600a9717
Hanlynui <hanyu.lin59@gmail.com>
2023-05-24 10:36:28 -0400

    commented out ssl

--- server/db/db.js
+++ server/db/db.js
@@ -13,13 +13,13 @@ if (process.env.LOGGING === "true") {
 }
 
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if (process.env.DATABASE_URL) {
-  config.dialectOptions = {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
-}
+// if (process.env.DATABASE_URL) {
+//   config.dialectOptions = {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   };
+// }
 
 // const db = new Sequelize(``, config);
 
